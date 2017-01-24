5044d688fa25228fdf4f5e7a39a18acac555a93d
Scott Lewis <slewis3827@gmail.com>
2017-01-24 14:06:49 -0600

    Add files via upload

---
+++ abtCode.R
@@ -0,0 +1,9 @@
+###aboutCode.R###
+
+fluidRow(h2(HTML("Want to build an app like this for <em>your</em> data?")),
+         h3(HTML("")),
+         h4(HTML("The website was built using <a href = 'http://shiny.rstudio.com/' target = 'blank'>Shiny Apps</a> and
+                 many R packages including ggplot2, ggvis, data.table, dplyr, and d3heatmap.")),
+         h4(HTML("All source code for the project is available on 
+                 <a href = 'https://github.com/5c077/Database-Template' target = 'blank'>Github</a>.")),
+         h4(HTML("")))
---
+++ global.R
@@ -0,0 +1,44 @@
+library(dplyr)
+library(tidyr)
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(d3heatmap)
+library(ggvis)
+library(ggplot2)
+# library(rCharts)
+library(dtplyr)
+library(data.table)
+#library(llamar)
+library(plotly)
+library(RColorBrewer)
+
+
+
+# Source javascript pagination code ---------------------------------------
+# Forked from https://github.com/wleepang/shiny-pager-ui
+# source('pagerui.R')
+
+# Import in the Muscle Transcriptome database -----------------------------
+
+# mt_source = src_sqlite('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_public_2015-11-08.sqlite3', create = FALSE)
+# data = tbl(mt_source, 'MT')
+
+data = read.csv('template.csv')
+#data = readRDS('major_tom.rds')
+GOs = readRDS('data/allOntologyTerms.rds')
+
+# Set the maximum of the expression, for the limits on the expr widget.
+maxInit <- max(data$expr)
+
+#allTissues = c('wild-type', 'chronic kidney disease')
+
+
+# List of tissues
+tissueList = list(
+'tissue A' = "tissue A", 'Tissue B' = 'Tissue B')
+
+allTissues = c(
+  
+               'Tissue A', 'Tissue B')
+
