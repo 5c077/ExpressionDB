08ddc97df96e42bde2c082d90e82017b53d46ce6
Scott Lewis <slewis3827@gmail.com>
2017-03-27 15:03:42 -0500

    Update abtCode.R

--- abtCode.R
+++ abtCode.R
@@ -5,5 +5,5 @@ fluidRow(h2(HTML("Want to build an app like this for <em>your</em> data?")),
          h4(HTML("The website was built using <a href = 'http://shiny.rstudio.com/' target = 'blank'>Shiny Apps</a> and
                  many R packages including ggplot2, ggvis, data.table, dplyr, and d3heatmap.")),
          h4(HTML("All source code for the project is available on 
-                 <a href = 'https://github.com/5c077/Database-Template' target = 'blank'>Github</a>.")),
-         h4(HTML("Found an issue? Report it <a href = 'https://github.com/5c077/Database-Template/issues' target = 'blank'>here</a>.")))
+                 <a href = 'https://github.com/5c077/ExpressionDB' target = 'blank'>Github</a>.")),
+         h4(HTML("Found an issue? Report it <a href = 'https://github.com/5c077/ExpressionDB/issues' target = 'blank'>here</a>.")))
