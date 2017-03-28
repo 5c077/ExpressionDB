05c3a1624dabf31ccd024933e5bad741abb6a266
Scott A. Lewis <slewis3827@gmail.com>
2017-03-28 10:43:21 -0500

    Update global.R

--- global.R
+++ global.R
@@ -19,10 +19,7 @@ library(RColorBrewer)
 # Forked from https://github.com/wleepang/shiny-pager-ui
 # source('pagerui.R')
 
-# Import in the Muscle Transcriptome database -----------------------------
-
-# mt_source = src_sqlite('~/Dropbox/Muscle Transcriptome Atlas/Website files/data/expr_public_2015-11-08.sqlite3', create = FALSE)
-# data = tbl(mt_source, 'MT')
+# Import in user-specified data set -----------------------------
 
 #specify the path to your custom .csv data frame. 'blank_frame.csv' is an example data frame with all of the required fields left blank, of course.
 data = read.csv('blank_frame.csv')
@@ -39,8 +36,6 @@ tissueList = list('tissue1' = 'tissue1',
 allTissues = c('tissue1', 'tissue2',
                'tissue3', 'tissue4'
                )
-
-# skelMuscles = c('DIA', 'EDL', 'EYE', 'SOL', 'TON','FDB', 'MAS', 'PLA', 'TAN', 'QUAD', 'GAS')
 # These are the tissues that will be selected initially -- when the application first starts up.
 selTissues = c(
                'tissue1', 'tissue2', 'tissue3'
