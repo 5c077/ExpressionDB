9036f6be76efeaed864c90b6e259c4670c66ea29
Scott A. Lewis <slewis3827@gmail.com>
2017-03-28 10:47:53 -0500

    Update summaryStats.R

--- summaryStats.R
+++ summaryStats.R
@@ -1,36 +1,3 @@
-# Widget showing the minimum, mean, and maximum expression value.
-# output$minExpr <- renderInfoBox({
-#   filteredData = filterData()
-#   
-#   print(dim())
-#   
-#   minVal = filteredData %>% 
-#     summarise(min = min(expr))
-#   
-#   # Find minimum value and which tissues have that value.
-#   minVal= minVal[[1]]
-#   
-#   iMin = which(filteredData$expr == minVal)
-#   
-#   
-#   if (length(iMin) > 2) {
-#     minGenes = paste0("in ", length(iMin),
-#                       " different tissues")
-#   } else {
-#     minTrans = filteredData$shortName[iMax]
-#     minTissue = filteredData$tissue[iMax]
-#     minGenes = paste0(minTrans, " (", minTissue, ")")
-#   }
-#   
-#   infoBox("minimum expression",
-#           minVal,
-#           minGenes,
-#           #               HTML(paste0(minVal, '<br>',"fdjks")),"rjek",
-#           icon = icon("chevron-down"),
-#           fill = TRUE
-#   )
-# })
-
 output$avgExpr <- renderInfoBox({
   filteredData = filterData()
   
