f78c58db81db121e8c019f54f1ff26980a27b168
Scott A. Lewis <slewis3827@gmail.com>
2017-07-14 10:47:07 -0500

    Delete truncate_gene_names.rb

--- truncate_gene_names.rb
+++
@@ -1,8 +0,0 @@
-z = []	
-	File.open("test.csv") do |f|
-		f.each_line do |line|
-		z << line.slice!(/uc......../)
-		#puts z.inspect
-	File.open('DONOTOPENUNTILFINISHED.csv', 'w') {|raw| raw.puts z}
-end
-end
