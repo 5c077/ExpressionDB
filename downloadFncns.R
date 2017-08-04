cd00bd14dbe2ee85ff6b4b59d0dc0407848f57cb
Laura Hughes <flaneuse@users.noreply.github.com>
2017-08-03 20:20:44 -0400

    remove onts; since nested, get screwed up on export

--- downloadFncns.R
+++ downloadFncns.R
@@ -6,6 +6,7 @@ output$downloadTable <- downloadHandler(
     filteredData = filterData()
     
     filteredData = filteredData %>% 
+      select_(paste0('-', ont_var)) %>% 
       spread(tissue, expr) 
     
     write.csv(filteredData, file)
