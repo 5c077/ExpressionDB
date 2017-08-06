0f0485a89aeaa074eec10382e19eac6c80ba8057
Laura Hughes <flaneuse@users.noreply.github.com>
2017-08-06 14:02:49 -0400

    round digits, add entrez link

--- calcPCA.R
+++ calcPCA.R
@@ -1,18 +1,22 @@
 calcPCA <- reactive({
   filteredData = filterData() %>% 
-    select_(data_unique_id, 'tissue', 'expr') %>% 
+    select_(data_unique_id, 'url', 'tissue', 'expr') %>% 
     spread(tissue, expr)
   
   geneNames = filteredData %>% 
-    select_(data_unique_id)
+    select(gene = url)
   
   pca_data  = filteredData %>% 
     ungroup() %>% 
-    select_(paste0('-', data_unique_id))
+    select_(paste0('-', data_unique_id), '-url')
   
   PCA = prcomp(pca_data, scale = TRUE, center = TRUE)
   
-  PCA$x = bind_cols(geneNames, data.frame(PCA$x))
+  
+  # round digits
+  PCA$x = bind_cols(geneNames, data.frame(PCA$x %>% round(., num_digits)))
+  
+  PCA$rotation = PCA$rotation %>% round(., num_digits)
   
   return(PCA)
 })
