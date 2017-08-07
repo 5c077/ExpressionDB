6a090043e2aef964b4c284100aa781dcd06a97f2
Laura Hughes <flaneuse@users.noreply.github.com>
2017-08-06 21:13:08 -0400

    clean additional onts

--- ontology_clean.R
+++ ontology_clean.R
@@ -1,15 +1,39 @@
 # function to clean up ontology files to use as example dataset
-# Ontologies exported by Scott Lewis on 1 August 2017
+# Ontologies exported by Scott Lewis on 4 August 2017
 # Removes extraneous columns and simplifies gene descriptions
 
 library(stringr)
 library(dplyr)
 library(tidyr)
+library(data.table)
 
-ont = read.csv('data/annot_expDB_human.csv', stringsAsFactors = FALSE)
+ont_files = list.files(path = 'data/') 
+ont_files = ont_files[ont_files%like% 'annot']
 
-ont2 = ont %>% 
+# human
+ont = read.csv('data/annot_expdb_humanV2.csv', stringsAsFactors = FALSE)
+
+human = ont %>% 
   separate(Gene.description, into = c('gene_description', 'source'), sep = ' \\[') %>% 
-  select(gene = Gene.name, gene_description, geneLink, GO)
+  select(gene_id_go = Symbol, gene_description, geneLink, GO)
 
-write.csv(ont2, 'data/sample_ontologies')  
+write.csv(human, 'data/annot_expdb_human2017-08.csv')  
+
+# mouse
+ont = read.csv('data/annot_expdb_mouseV2.csv', stringsAsFactors = FALSE)
+
+mouse = ont %>% 
+  separate(Gene.description, into = c('gene_description', 'source'), sep = ' \\[') %>% 
+  select(gene_id_go = Symbol, gene_description, geneLink, GO)
+
+write.csv(mouse, 'data/annot_expdb_mouse2017-08.csv')  
+
+# zee rats
+ont = read.csv('data/annot_expdb_ratV2.csv', stringsAsFactors = FALSE)
+
+rat = ont %>% 
+  separate(Gene.description, into = c('gene_description', 'source'), sep = ' \\[') %>% 
+  select(gene_id_go = Symbol, gene_description, geneLink, GO)
+
+write.csv(rat, 'data/annot_expdb_rat2017-08.csv')  
+
